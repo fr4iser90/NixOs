3d1589ec3a2091f7dbecb4b20cb989d7f9b6b4b2
fr4iser90 <pa.boe90@gmail.com>
2024-07-15 01:18:17 +0200

    chore: Refactor shell initialization and environment setup

--- nixos/modules/homemanager/homeMainUser.nix
+++ nixos/modules/homemanager/homeMainUser.nix
@@ -3,21 +3,15 @@
 
 let
   env = import ../../env.nix;
-  #shellInitModule = import ./shellInit/index.nix { inherit pkgs lib; defaultShell = env.defaultShell; };
+  shellInitModule = import ./shellInit/index.nix { inherit pkgs lib; defaultShell = env.defaultShell; };
 in
 {
   home.stateVersion = "24.05";
   home.username = user;
   home.homeDirectory = lib.mkForce "/home/${user}";
-  programs.fish.enable = true;
-  programs.fish.interactiveShellInit = ''
-    function fish_prompt
-      echo -n (prompt_pwd)
-      echo -n ' > '
-    end
-  '';
+  
 
-  #imports = [ shellInitModule ];
+  imports = [ shellInitModule ];
 
   home.sessionVariables = {
     # Add session variables here
--- nixos/modules/homemanager/shellInit/bashInit.nix
+++ nixos/modules/homemanager/shellInit/bashInit.nix
@@ -1,16 +1,9 @@
 #/etc/nixos/modules/homemanager/shellInit/bashInit.nix
  {
    programs.bash = {
-     enable = true;
-     interactiveShellInit = ''
-       export PS1="\w > "
-     '';
-   };
+    enable = true;
+    initExtra = ''
+      export PS1="\w > "
+    '';
+  };
  }
-
-#{
-#  programs.bash.enable = true;
-#  programs.bash.interactiveShellInit = ''
-#      export PS1="\w > "
-#    '';
-#}
