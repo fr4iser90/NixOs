0eeb6c54c645912db49a6e141d4ddc62f25bba76
fr4iser90 <pa.boe90@gmail.com>
2024-07-17 23:13:13 +0200

    feat: Add desktop integration module

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,6 +12,7 @@ in {
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix # Don't touch!
       ./modules/bootloader/bootloader.nix
+      ./modules/desktion/index.nix
       ./modules/networking/networking.nix
       ./modules/users/index.nix
 #      ./modules/system-services/utility/garbagecollector.nix
