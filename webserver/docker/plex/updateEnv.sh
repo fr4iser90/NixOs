32e591ca3d7b2ed0e8a3fa5ac725e98c838326b5
fr4iser90 <pa.boe90@gmail.com>
2024-07-24 22:02:20 +0200

    chore: Update plex.env with PUID and PGID variables

---
+++ webserver/docker/plex/updateEnv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BASE_DIR="/home/docker/docker/plex"
+COMPOSE_FILE="docker-compose.yml"
+
+# Function to get the UID and GID of the current user
+get_uid_gid() {
+  USER_UID=$(id -u)
+  USER_GID=$(id -g)
+}
+
+# Function to update the Docker Compose file
+update_compose_file() {
+  if [ -f "$BASE_DIR/$COMPOSE_FILE" ]; then
+    echo "Updating $COMPOSE_FILE in Plex with UID=$USER_UID and GID=$USER_GID"
+    sed -i "s|PUID=[0-9]*|PUID=$USER_UID|" "$BASE_DIR/$COMPOSE_FILE"
+    sed -i "s|PGID=[0-9]*|PGID=$USER_GID|" "$BASE_DIR/$COMPOSE_FILE"
+  else
+    echo "File $BASE_DIR/$COMPOSE_FILE does not exist" >&2
+    exit 1
+  fi
+}
+
+# Main execution
+get_uid_gid
+update_compose_file
+
+echo "Plex Docker Compose file has been updated."
