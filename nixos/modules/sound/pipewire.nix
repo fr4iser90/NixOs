0e360be3177ee687991e113c957ff92e5ac3568d
fr4iser90 <pa.boe90@gmail.com>
2024-08-06 18:27:57 +0200

    feat: Update sound configuration with PipeWire
    The sound configuration has been updated to use PipeWire instead of PulseAudio. This change ensures that PulseAudio is disabled and PipeWire is enabled. Additionally, the system packages have been updated to include necessary dependencies for PipeWire.

--- nixos/modules/sound/pipewire.nix
+++ nixos/modules/sound/pipewire.nix
@@ -1,9 +1,11 @@
-# /etc/nixos/modules/sound/sound.nix
+# /etc/nixos/modules/sound/pipewire.nix
 
 { config, pkgs, ... }:
 
 {
-  hardware.pulseaudio.enable = false; # Stelle sicher, dass PulseAudio aktiviert ist
+  # Stelle sicher, dass PulseAudio deaktiviert ist, weil wir PipeWire verwenden
+  hardware.pulseaudio.enable = false;
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -11,39 +13,15 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
-
-    configPackages = [
-      (pkgs.writeTextFile {
-        name = "custom-pipewire-config";
-        destination = "/etc/pipewire/pipewire.conf";
-        text = ''
-          context.modules = [
-            { name = libpipewire-module-rtkit }
-            { name = libpipewire-module-alsa-source args = { device = "hw:2,0" } }
-            { name = libpipewire-module-alsa-source args = { device = "hw:3,0" } }
-          ];
-          default.clock.allowed-rates = [ 48000 44100 ];
-          default.clock.rate = 48000;
-          default.clock.quantum = 1024;
-          default.clock.min-quantum = 32;
-          default.clock.max-quantum = 8192;
-        '';
-      })
-    ];
   };
 
-  # Erstelle die PulseAudio-Konfigurationsdatei
-  environment.etc."pulse/default.pa".text = ''
-    # Unload all existing sources
-    .ifexists module-udev-detect.so
-    unload-module module-udev-detect
-    .endif
-
-    # Load only the specified sources
-    load-module module-alsa-source device=hw:2,0
-    load-module module-alsa-source device=hw:3,0
-
-    # Optionally, set the default source
-    set-default-source alsa_input.usb-Torch_Streaming_Microphone
-  '';
+  environment.systemPackages = with pkgs; [
+    pipewire
+    pavucontrol  # PulseAudio Volume Control
+    alsa-utils   # ALSA Utilities
+    qpwgraph     # Graphisches Frontend für PipeWire
+    mda_lv2      # LADSPA-Plugins
+    calf         # Weitere Audio-Plugins
+    noisetorch   # Noisecanceling   ( need sys cap, I installed it locally via github)
+  ];
 }
--- nixos/modules/sound/pulseaudio.nix
+++ nixos/modules/sound/pulseaudio.nix
@@ -1,6 +1,24 @@
+# /etc/nixos/modules/sound/pulseaudio.nix
+
 { config, pkgs, ... }:
 
 {
-  hardware.pulseaudio.enable = true; # Ensure PulseAudio is enabled
+  hardware.pulseaudio.enable = true; # Stelle sicher, dass PulseAudio aktiviert ist
   security.rtkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    pulseaudio
+    pavucontrol    # PulseAudio Volume Control
+    alsa-utils     # ALSA Utilities
+    qpwgraph       # Graphisches Frontend für PulseAudio/PipeWire
+    mda_lv2        # LADSPA-Plugins
+    calf           # Weitere Audio-Plugins
+    noisetorch     # Noisecanceling
+  ];
+
+  # PulseAudio Konfiguration
+  hardware.pulseaudio = {
+    package = pkgs.pulseaudioFull; # Verwende das vollständige PulseAudio-Paket mit allen Plugins
+    support32Bit = true; # Falls du 32-Bit-Unterstützung benötigst
+  };
 }
